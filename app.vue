f40c3175a38f66fbc965c36cf37813fcc3a0f7b9
ryck-developer <88977217+ryck-developer@users.noreply.github.com>
2023-01-04 23:10:43 -0300

    1 file modified

--- app.vue
+++ app.vue
@@ -1,18 +1,18 @@
 <template>
   <div ref="container">
     <img
-      width="400"
+      width="200"
       src="https://media-public.canva.com/MADE6lOJ6h0/1/thumbnail_large-1.png"
       alt=""
     />
     <img
       ref="roleta"
-      width="400"
+      width="300"
       src="https://media-public.canva.com/RTx-w/MADzv3RTx-w/1/tl.png"
       alt=""
     />
     <img
-      width="300"
+      width="200"
       src="https://media-public.canva.com/bujNU/MAFEIBbujNU/1/tl.png"
       alt=""
     />
@@ -22,13 +22,17 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      isTeste: true,
+      elapsedTime: 0,
+    };
   },
   mounted() {
     // Armazena a referência do componente em uma variável
     var vm = this;
-
-    setInterval(function () {
+    var start = new Date();
+    // Armazena o retorno do setInterval em uma variável
+    this.intervalId = setInterval(function () {
       vm.$refs.roleta.style.transform = 'rotate(360deg)';
 
       // Para a roleta aleatoriamente em um ângulo
@@ -37,6 +41,15 @@ export default {
         vm.$refs.roleta.style.transform = 'rotate(' + randomAngle + 'deg)';
       }, 3000); // 3000 é o tempo em milissegundos que a roleta leva para parar
     }, 0); // 0 é o tempo em milissegundos para a execução da função
+
+    var end = new Date();
+    // Calcula a diferença entre as duas datas em milissegundos
+    this.elapsedTime = end - start;
+
+    if (this.elapsedTime == 1000) {
+      clearInterval(this.intervalId);
+      console.log(this.elapsedTime);
+    }
   },
 };
 </script>
